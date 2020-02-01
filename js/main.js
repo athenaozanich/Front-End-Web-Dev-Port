c2dbcb7b5bf9c6074f96f356d9721db87f4dde1c
Athena Ozanich <athenaozanich@gmail.com>
2020-02-01 18:08:17 -0500

    syncing and merging changes

--- js/main.js
+++ js/main.js
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', function() {
-
-   //Wait for document to be ready
-   let projectCards = [];
-   let vertPortWrapper = document.querySelector(".vert-wrapper");
-   let projectCard;
-   let parsedProjectCard;
-   let modal;
-   //get data from db
-   fetch('./js/portData.json')
-   .then(function(response) {
-     return response.json();
-   })
-   .then(function(myJson) {
-
-     //store data array
-     let projects = myJson.portItems;
-     //console.log(projects);
-     //loop through data
-     for (var i = 0; i < projects.length; i++) {
-       //create variable for project card
-       projectCard =
-       `<aside class="port-item card__${i}">
-       <span class="close">X</span>
-       <h4>${projects[i].name}</h4>
-       <img src="imgs/${projects[i].img}" alt="${projects[i].name}" />
-       <div><canvas id="barGraph__${i}"> </canvas></div>
-       <span>Click to read more</span>
-       <p>
-       ${projects[i].desc}
-       </p>
-
-       <a href="${projects[i].url}">Site</a>
-       <a href="${projects[i].repo}">Github</a>
-       </aside>`;
-       //parse string into html
-       parsedProjectCard = $.parseHTML(projectCard);
-       projectCards.push(parsedProjectCard[0]);
-       //append card to DOM
-       vertPortWrapper.appendChild(projectCards[i]);
-     }
-     return projectCards;
-   });
-
-
-   vertPortWrapper.addEventListener('click', e => {
-     const projectCard = e.target.closest(".port-item");
-     if (e.target.className != 'close' && e.target.tagName !== 'H6'){
-       modalHandler(projectCard);
-     }
-
-   });
-
-=======
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
 //Wait for document to be ready
 const vertPortWrapper = document.querySelector(".vert-wrapper");
 let projectIdx = 0;
@@ -143,10 +85,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
    });
-<<<<<<< HEAD
->>>>>>> 5d9d4b7ce42e6381b5f150d11ae6b6c3f9e02316
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
 
    function modalHandler(projectCard) {
      projectCard.classList.add('modal');
@@ -157,12 +95,5 @@ document.addEventListener('DOMContentLoaded', function() {
      })
 
    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
   pullData();
->>>>>>> 5d9d4b7ce42e6381b5f150d11ae6b6c3f9e02316
-=======
-  pullData();
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
 }, false);
--- js/web_app_charts/main.js
+++ js/web_app_charts/main.js
@@ -4,35 +4,17 @@ function addGraphs(){
 
   for (var i = 0; i < barGraphs.length; i++) {
     let ctx = barGraphs[i].getContext('2d');
-<<<<<<< HEAD
-<<<<<<< HEAD
-    console.log(ctx);
-=======
->>>>>>> 5d9d4b7ce42e6381b5f150d11ae6b6c3f9e02316
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
+
     // your code here
 
     var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-=======
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
         labels: ['HTML', 'CSS', 'JS/JQ', 'SASS', 'WCAG', 'REACT'],
         datasets: [{
           label: 'Skills Utilized',
           data: [100, 100, 30, 20, 30, 0],
-<<<<<<< HEAD
->>>>>>> 5d9d4b7ce42e6381b5f150d11ae6b6c3f9e02316
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
+
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -56,20 +38,10 @@ function addGraphs(){
         scales: {
           yAxes: [{
             ticks: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-              beginAtZero: true
-=======
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
               beginAtZero: true,
               min: 0,
               max: 100,
               callback: function(value){return value+ "%"}
-<<<<<<< HEAD
->>>>>>> 5d9d4b7ce42e6381b5f150d11ae6b6c3f9e02316
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
             }
           }]
         }
@@ -77,13 +49,6 @@ function addGraphs(){
     });
   }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-addGraphs();
-=======
 setTimeout(function(){
   addGraphs();
 },1000);
->>>>>>> 5d9d4b7ce42e6381b5f150d11ae6b6c3f9e02316
-=======
->>>>>>> ccb5ce12db3d3004a61ebf4d8eea78a950f7ee14
